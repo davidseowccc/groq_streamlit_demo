2630b9d713865dc3ae78d80d2aa9e14d88ed4e65
davidseowccc <62555082+davidseowccc@users.noreply.github.com>
2024-08-15 12:26:36 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -54,7 +54,7 @@ with col1:
         "Choose a model:",
         options=list(models.keys()),
         format_func=lambda x: models[x]["name"],
-        index=8  # row 8
+        index=7  # row 8
     )
 
 # Detect model change and clear chat history if model has changed
@@ -72,7 +72,7 @@ with col2:
         min_value=512,  # Minimum value to allow some flexibility
         max_value=max_tokens_range,
         # Default value or max allowed if less
-        value=min(min_value, max_tokens_range),
+        value=min(4096, max_tokens_range),
         step=512,
         help=f"Adjust the maximum number of tokens (words) for the model's response. Max for selected model: {max_tokens_range}"
     )
